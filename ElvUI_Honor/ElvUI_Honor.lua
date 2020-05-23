815f172c8346b12eb904bb14aff232f773651f3a
Hydra <ryanandrews135@gmail.com>
2020-05-22 22:33:10 -0600

    Add lifetime kills, and fix color

--- ElvUI_Honor/ElvUI_Honor.lua
+++ ElvUI_Honor/ElvUI_Honor.lua
@@ -7,7 +7,7 @@ local UnitHonorMax = UnitHonorMax
 local UnitHonorLevel = UnitHonorLevel
 local BreakUpLargeNumbers = BreakUpLargeNumbers
 local Honor = HONOR
-local String = ""
+local String = "%s: %s / %s"
 local Panel
 
 local OnEvent = function(self, event, unit)
@@ -26,12 +26,13 @@ end
 
 local OnEnter = function(self)
 	DT:SetupTooltip(self)
-
+	
 	local Honor = UnitHonor("player")
 	local MaxHonor = UnitHonorMax("player")
 	local Percent = floor((Honor / MaxHonor * 100 + 0.05) * 10) / 10
 	local Remaining = MaxHonor - Honor
 	local RemainingPercent = floor((Remaining / MaxHonor * 100 + 0.05) * 10) / 10
+	local Kills = GetPVPLifetimeStats()
 	
 	DT.tooltip:AddLine(format(HONOR_LEVEL_TOOLTIP, UnitHonorLevel("player")))
 	DT.tooltip:AddLine(" ")
@@ -42,17 +43,23 @@ local OnEnter = function(self)
 	DT.tooltip:AddLine("Remaining honor")
 	DT.tooltip:AddDoubleLine(format("%s", BreakUpLargeNumbers(Remaining)), format("%s%%", RemainingPercent), 1, 1, 1, 1, 1, 1)
 	
+	if (Kills > 0) then
+		DT.tooltip:AddLine(" ")
+		DT.tooltip:AddLine(HONORABLE_KILLS)
+		DT.tooltip:AddLine(Kills, 1, 1, 1)
+	end
+	
 	DT.tooltip:Show()
 end
 
 local ValueColorUpdate = function(hex)
 	String = strjoin("", "%s: ", hex, "%s|r / ", hex, "%s|r")
 	
-	if Panel then
+	if (Panel ~= nil) then
 		OnEvent(Panel)
 	end
 end
 
 E.valueColorUpdateFuncs[ValueColorUpdate] = true
 
-DT:RegisterDatatext("Honor", {"HONOR_XP_UPDATE", "HONOR_LEVEL_UPDATE"}, OnEvent, nil, OnClick, OnEnter, nil, Honor)
+DT:RegisterDatatext("Honor", {"HONOR_XP_UPDATE", "HONOR_LEVEL_UPDATE", "PLAYER_ENTERING_WORLD"}, OnEvent, nil, OnClick, OnEnter, nil, Honor)
